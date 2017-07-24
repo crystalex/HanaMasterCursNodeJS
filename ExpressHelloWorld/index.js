5fb6d3caaf0b686925aa3f8a8d31ec7b20068075
GLOBAL\I313572 <alexandru.zaharescu@sap.com>
2017-07-24 11:28:20 +0200

    express backend

---
+++ ExpressHelloWorld/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World');
+
+});
+
+ app.post('/', function(req,res) { 
+     res.send('No Post method available'); 
+});
+
+app.get('/hello/world', function (req, res) {
+  res.send('Hello World 2');
+
+});
+
+app.listen(3000);
+
+//Console will print the server info
+console.log('Server running at http://127.0.0.1:3000/');
+
--- HelloWorld/index.js
+++ HelloWorld/index.js
@@ -1,2 +1,36 @@
 /* Hello World! program in Node.js */
-console.log("Hello World!");
+"use strict";
+
+/* Required module */
+var mHttp = require("http");
+
+// Variables
+var iPort = 8081;
+var sMessage = "Hello World";
+
+var oResponse = {
+"returnCode" : "1",
+"returnMessage" : "Everything OK",
+"returnData" : "null"
+};
+
+//Create Http Server
+var oServer = mHttp.createServer(function (req, res) {
+console.log("\r\nRequest received");
+
+res.writeHead(200, {'Content-type': 'text/plain'});
+
+res.write(JSON.stringify(oResponse));
+//res.end("Raspuns primit");
+
+res.end();
+console.log("Response sent !");
+});
+oServer.listen(iPort);
+
+//Console will print the server info
+console.log('Server running at http://127.0.0.1:' + iPort + '/' );
+
+sMessage = sMessage + "!";
+
+console.log(sMessage); 
