3b78d176290565cb09967792fe87b533102b70f3
GLOBAL\I313572 <alexandru.zaharescu@sap.com>
2017-07-25 11:11:45 +0200

    no message

---
+++ UnitTestExample/test/JsonParseTests.js
@@ -0,0 +1,9 @@
+describe("passwordValidator", function () {
+    describe(".validPassword(password)", function () {
+        it("should have minimum 8 chars length", function () {
+            // Test Goes Here
+        });
+
+    });
+
+});
