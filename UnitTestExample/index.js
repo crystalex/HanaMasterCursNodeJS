85f140d08947c7d1ae126b7cd3d9d8a51bb32182
GLOBAL\I313572 <alexandru.zaharescu@sap.com>
2017-07-25 11:28:30 +0200

    12:30

--- UnitTestExample/index.js
+++ UnitTestExample/index.js
@@ -2,7 +2,7 @@ console.log("Unit test example start");
 
 var myValidator = require("./modules/passwordValidator");
 
-var password = "test2017";
+var password = "test";
 var valid = myValidator.validPassword(password);
 
-console.log("Your password " + password + " is :" + valid);
+console.log("Your password " + password + " is valid :" + valid);
--- UnitTestExample/test/passwordValidatorTests.js
+++ UnitTestExample/test/passwordValidatorTests.js
@@ -1,3 +1,6 @@
+var chai = require("chai");
+var passwordValidator = require("../modules/passwordValidator");
+
 // module level
 describe("passwordValidator", function () {
 
@@ -6,9 +9,39 @@ describe("passwordValidator", function () {
 
         //function test 1
         it("should have minimum 8 chars length", function () {
-            // Test Goes Here
-        });
+            
+            //implementation test with known wrong value
+            var wrongPass = "1234567";
+            var valid = passwordValidator.validPassword(wrongPass);
+            
+            //chai check wrong
+            chai.expect(valid).to.be.equal(false);
 
+            //test with good value
+            var goodPassword = "12345678";
+            var valid2 = passwordValidator.validPassword(goodPassword);
+
+            // chai check good
+            chai.expect(valid).to.be.equal(true);
+        });
+        
+        //function test 2
+        it("it should not contain 'password' inside", function () {
+            
+            //implementation test with known wrong value
+            var wrongPass = "password1";
+            var valid2 = passwordValidator.validPassword(wrongPass);
+            
+            //chai check wrong
+            chai.expect(valid2).to.be.equal(false);
+
+            //test with good value
+            var goodPassword = "blabla";
+            var valid2 = passwordValidator.validPassword(goodPassword);
+
+            // chai check good
+            chai.expect(valid2).to.be.equal(true);
+        });
     });
 
 });
